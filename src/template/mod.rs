d7f4986ccbb2b3e7a6fdf1a1261b21b6aa25a0f4
Christian Iversen <ci@iversenit.dk>
2024-11-06 20:17:28 +0100

    + Implement logging functions as template functions

--- src/template/mod.rs
+++ src/template/mod.rs
@@ -1,10 +1,32 @@
 use minijinja::syntax::SyntaxConfig;
-use minijinja::{Environment, ErrorKind, UndefinedBehavior};
+use minijinja::value::Rest;
+use minijinja::{Environment, ErrorKind, State, UndefinedBehavior};
 
 use crate::RaptorResult;
 
-fn hello(arg: String) {
-    log::info!("Hello {:?}", arg);
+#[allow(clippy::needless_pass_by_value)]
+fn trace(state: &State, args: Rest<String>) {
+    log::trace!("{} :: {}", state.name(), args.join(" "));
+}
+
+#[allow(clippy::needless_pass_by_value)]
+fn debug(state: &State, args: Rest<String>) {
+    log::debug!("{} :: {}", state.name(), args.join(" "));
+}
+
+#[allow(clippy::needless_pass_by_value)]
+fn info(state: &State, args: Rest<String>) {
+    log::info!("{} :: {}", state.name(), args.join(" "));
+}
+
+#[allow(clippy::needless_pass_by_value)]
+fn warning(state: &State, args: Rest<String>) {
+    log::warn!("{} :: {}", state.name(), args.join(" "));
+}
+
+#[allow(clippy::needless_pass_by_value)]
+fn error(state: &State, args: Rest<String>) {
+    log::error!("{} :: {}", state.name(), args.join(" "));
 }
 
 pub fn make_environment<'a>() -> RaptorResult<Environment<'a>> {
@@ -28,7 +50,11 @@ pub fn make_environment<'a>() -> RaptorResult<Environment<'a>> {
             .build()?,
     );
 
-    env.add_function("hello", hello);
+    env.add_function("trace", trace);
+    env.add_function("debug", debug);
+    env.add_function("info", info);
+    env.add_function("warning", warning);
+    env.add_function("error", error);
 
     Ok(env)
 }
