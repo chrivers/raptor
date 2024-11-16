6a473f9c449884b8163db393d61979d98e274da0
Christian Iversen <ci@iversenit.dk>
2024-11-16 22:31:28 +0100

    + Implemented demo program for nspawn-client

---
+++ src/bin/nspawn-test.rs
@@ -0,0 +1,33 @@
+use std::os::unix::net::UnixListener;
+
+use log::{error, info};
+use raptor::client::{FramedRead, FramedWrite, Response};
+use raptor::{client::Request, RaptorResult};
+
+fn main() -> RaptorResult<()> {
+    colog::init();
+    let socket_name = std::env::var("RAPTOR_NSPAWN_SOCKET")?;
+    let listen = UnixListener::bind(socket_name)?;
+
+    let (mut stream, _addr) = listen.accept()?;
+
+    let req = Request::Run {
+        arg0: String::from("sh"),
+        argv: ["/bin/sh", "-c", "id"]
+            .into_iter()
+            .map(ToOwned::to_owned)
+            .collect(),
+    };
+
+    info!("writing frame: {req:?}");
+    stream.write_framed(req)?;
+
+    let resp: Response = stream.read_framed()?;
+
+    match resp {
+        Response::Err(err) => error!("Error: {err}"),
+        Response::Ok(res) => info!("Success: {res}"),
+    }
+
+    Ok(())
+}
