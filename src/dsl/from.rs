1e0cad2c02eb6eb60db34162afa758131783cd6b
Christian Iversen <ci@iversenit.dk>
2024-11-17 22:19:33 +0100

    + Implement Debug for InstFrom

--- src/dsl/from.rs
+++ src/dsl/from.rs
@@ -1,4 +1,12 @@
-#[derive(Clone, Debug)]
+use std::fmt::Debug;
+
+#[derive(Clone)]
 pub struct InstFrom {
     pub from: String,
+}
+
+impl Debug for InstFrom {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "FROM {}", self.from)
+    }
 }
