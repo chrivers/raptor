98b8d2134a41178890ca55b50c8f2d935d1d6252
Christian Iversen <ci@iversenit.dk>
2024-11-17 22:19:26 +0100

    + Implement Debug for InstRun

--- src/dsl/run.rs
+++ src/dsl/run.rs
@@ -1,4 +1,12 @@
-#[derive(Clone, Debug)]
+use std::fmt::Debug;
+
+#[derive(Clone)]
 pub struct InstRun {
     pub run: Vec<String>,
+}
+
+impl Debug for InstRun {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "RUN {}", self.run.join(" "))
+    }
 }
