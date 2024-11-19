ab05af07e84f953a50db7fcca8333678385e0708
Christian Iversen <ci@iversenit.dk>
2024-11-19 22:15:18 +0100

    * Make Executor support Instruction::Write, Instruction::Copy (with progress bar support), and Instruction::Run

--- src/program/executor.rs
+++ src/program/executor.rs
@@ -1,19 +1,34 @@
+use std::fs::File;
+use std::io::{BufWriter, Write};
 use std::process::Command;
 
-use crate::{
-    dsl::{Instruction, Statement},
-    sandbox::Sandbox,
-    RaptorResult,
-};
+use camino::Utf8PathBuf;
+use indicatif::{ProgressBar, ProgressStyle};
+
+use crate::dsl::{Instruction, Statement};
+use crate::sandbox::Sandbox;
+use crate::RaptorResult;
 
 pub struct Executor {
     sandbox: Sandbox,
 }
 
 impl Executor {
+    const BUFFER_SIZE: usize = 128 * 1024;
+
+    const PROGRESS_STYLE: &str = "[{elapsed_precise}] {bar:40.cyan/blue} {bytes:>7}/{total_bytes:7} {binary_bytes_per_sec} {msg}";
+
     #[must_use]
     pub const fn new(sandbox: Sandbox) -> Self {
         Self { sandbox }
+    }
+
+    fn progress_bar(len: u64) -> ProgressBar {
+        let style = ProgressStyle::with_template(Self::PROGRESS_STYLE)
+            .unwrap()
+            .progress_chars("#>-");
+
+        indicatif::ProgressBar::new(len).with_style(style)
     }
 
     pub fn handle(&mut self, stmt: &Statement) -> RaptorResult<()> {
@@ -23,16 +38,29 @@ impl Executor {
             }
             Instruction::Copy(inst) => {
                 info!("{:?}", inst);
+                let mut src = File::open(&inst.srcs[0])?;
+                let fd = self
+                    .sandbox
+                    .create_file_handle(&Utf8PathBuf::from(&inst.dest))?;
+
+                let pb = Self::progress_bar(src.metadata()?.len());
+                let mut dst = BufWriter::with_capacity(Self::BUFFER_SIZE, pb.wrap_write(fd));
+
+                std::io::copy(&mut src, &mut dst)?;
             }
             Instruction::Render(inst) => {
                 info!("{:?}", inst);
             }
             Instruction::Write(inst) => {
                 info!("{:?}", inst);
+                let mut fd = self
+                    .sandbox
+                    .create_file_handle(&Utf8PathBuf::from(&inst.dest))?;
+                fd.write_all(inst.body.as_bytes())?;
             }
             Instruction::Run(inst) => {
                 debug!("{:?}", inst);
-                /* self.sandbox.run(&inst.run)?; */
+                self.sandbox.run(&inst.run)?;
             }
 
             Instruction::Invoke(inst) => {
@@ -45,7 +73,7 @@ impl Executor {
         Ok(())
     }
 
-    pub fn finish(mut self) -> RaptorResult<()> {
+    pub fn finish(self) -> RaptorResult<()> {
         self.sandbox.close()
     }
 }
