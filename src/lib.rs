4006c61114a3b7a56cd1cc050b04e32d0a42bb66
Christian Iversen <ci@iversenit.dk>
2024-11-21 20:19:57 +0100

    * Move clippy lint settings to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,3 @@
-#![warn(
-    clippy::all,
-    clippy::correctness,
-    clippy::pedantic,
-    clippy::cargo,
-    clippy::nursery,
-    clippy::perf,
-    clippy::style
-)]
-#![allow(
-    clippy::multiple_crate_versions,
-    clippy::missing_errors_doc,
-    clippy::missing_panics_doc,
-    clippy::module_name_repetitions,
-    clippy::module_inception,
-    clippy::similar_names
-)]
-
 #[macro_use]
 extern crate log;
 
