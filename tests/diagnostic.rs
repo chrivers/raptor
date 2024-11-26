3a861fdb26168cb3dcf69e6db372619cb7af4dfe
Christian Iversen <ci@iversenit.dk>
2024-11-26 18:50:47 +0100

    * Reimplement better, simpler custom diagnostic tests, using libmimic-test

---
+++ tests/diagnostic.rs
@@ -0,0 +1,56 @@
+use std::{
+    io::Read,
+    process::{Command, Stdio},
+};
+
+use camino::Utf8Path;
+use libtest_mimic::{Arguments, Trial};
+use raptor::RaptorResult;
+
+fn run_raptor(filename: &Utf8Path) -> RaptorResult<String> {
+    let mut proc = Command::new("cargo")
+        .arg("run")
+        .arg("-q")
+        .arg("--bin")
+        .arg("raptor")
+        .arg(filename)
+        .stdin(Stdio::null())
+        .stdout(Stdio::piped())
+        .spawn()?;
+    let mut stdout = proc.stdout.take().unwrap();
+    let mut message = String::new();
+    stdout.read_to_string(&mut message)?;
+
+    Ok(message)
+}
+
+#[allow(clippy::case_sensitive_file_extension_comparisons)]
+fn main() -> RaptorResult<()> {
+    let args = Arguments::from_args();
+
+    let mut tests = vec![];
+
+    let cases = Utf8Path::new("tests/cases/error").read_dir_utf8()?;
+
+    for dent in cases {
+        let dent = dent?;
+        if !dent.file_name().ends_with(".rapt") {
+            continue;
+        }
+        let refpath = dent.path().with_extension("out");
+
+        let path = dent.path().to_owned();
+        let test = Trial::test(dent.file_name(), move || {
+            let refdata = std::fs::read_to_string(refpath)?;
+            let newdata = run_raptor(&path)?;
+
+            assert_eq!(refdata, newdata);
+            Ok(())
+        });
+
+        tests.push(test);
+    }
+
+    /* run all tests using libtest-mimic */
+    libtest_mimic::run(&args, tests).exit()
+}
