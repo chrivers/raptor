b826cabdb571768acfae83dfae63820c3bf4733b
Christian Iversen <ci@iversenit.dk>
2024-12-07 14:45:47 +0100

    + Add tests for nspawn-client (without sandbox)

---
+++ tests/nspawn-client.rs
@@ -0,0 +1,60 @@
+use std::os::unix::net::UnixListener;
+use std::process::Command;
+
+use camino_tempfile::{NamedUtf8TempFile, Utf8TempDir};
+use nix::errno::Errno;
+
+use raptor::sandbox::{SandboxClient, SandboxExt};
+use raptor::{RaptorError, RaptorResult};
+
+const TEST_DATA: &[u8] = b"Raptortest\n";
+
+fn spawn_client() -> RaptorResult<SandboxClient> {
+    let exe = std::env::current_exe().unwrap();
+    let deps = exe.parent().unwrap().parent().unwrap();
+    let client = deps.join("nspawn-client");
+
+    let tempdir = Utf8TempDir::new()?;
+    let socket_path = tempdir.path().join("raptor-test");
+    let listen = UnixListener::bind(&socket_path)?;
+
+    let proc = Command::new(client)
+        .env("RAPTOR_NSPAWN_LOG_LEVEL", "off")
+        .env("RAPTOR_NSPAWN_SOCKET", socket_path.as_str())
+        .spawn()?;
+
+    let conn = listen.accept()?.0;
+
+    Ok(SandboxClient::new(proc, conn))
+}
+
+#[test]
+fn chdir_tmp() -> RaptorResult<()> {
+    let mut sc = spawn_client()?;
+    sc.chdir("/tmp")?;
+    sc.close()
+}
+
+#[test]
+fn write_file_over_dir() -> RaptorResult<()> {
+    let mut sc = spawn_client()?;
+    match sc.create_file("/tmp".into(), None, None).unwrap_err() {
+        RaptorError::SandboxRequestError(Errno::EISDIR) => {}
+        err => panic!("unexpected error: {err}"),
+    }
+    sc.close()
+}
+
+#[test]
+fn client_write_file() -> RaptorResult<()> {
+    let mut sc = spawn_client()?;
+
+    let tmpfile = NamedUtf8TempFile::new()?;
+    let path = tmpfile.path();
+
+    sc.write_file(path, None, None, TEST_DATA)?;
+
+    assert_eq!(std::fs::read(path)?, TEST_DATA);
+
+    sc.close()
+}
