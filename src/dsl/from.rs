6528c8f3910a457884d8be10aec491e8d1ad6e5b
Christian Iversen <ci@iversenit.dk>
2025-09-09 21:04:39 +0200

    + Add FromSource dsl element

--- src/dsl/from.rs
+++ src/dsl/from.rs
@@ -3,6 +3,12 @@ use std::fmt::{self, Debug, Display};
 use crate::print::Theme;
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub enum FromSource {
+    Plain(String),
+    Docker(String),
+}
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
 pub struct InstFrom {
     pub from: String,
 }
