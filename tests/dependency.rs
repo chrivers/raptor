50156309cd6198eb11f8023dbef4fd2ea44eca41
Christian Iversen <ci@iversenit.dk>
2025-09-13 10:11:07 +0200

    * Cleaned up dependency testing code

--- tests/dependency.rs
+++ tests/dependency.rs
@@ -12,23 +12,23 @@ use raptor::program::Loader;
 use raptor::RaptorResult;
 
 trait Writable {
-    fn as_string(self) -> String;
+    fn into_string(self) -> String;
 }
 
 impl Writable for &str {
-    fn as_string(self) -> String {
+    fn into_string(self) -> String {
         self.to_string()
     }
 }
 
 impl<const N: usize> Writable for [&str; N] {
-    fn as_string(self) -> String {
-        self[..].as_string()
+    fn into_string(self) -> String {
+        self[..].into_string()
     }
 }
 
 impl Writable for &[&str] {
-    fn as_string(self) -> String {
+    fn into_string(self) -> String {
         let mut res = self.join("\n");
         res.push('\n');
         res
@@ -65,15 +65,14 @@ impl Tester {
         Ok(res)
     }
 
-    fn step(
-        &mut self,
-        description: &str,
-        func: impl Fn(&mut Self) -> RaptorResult<()>,
-    ) -> RaptorResult<()> {
-        func(self)?;
+    fn step(&mut self, act: &str, fun: impl Fn(&mut Self) -> RaptorResult<()>) -> RaptorResult<()> {
+        fun(self)?;
 
         let hash = self.program_hash()?;
-        assert_ne!(self.hash, hash, "{description}");
+        if self.hash == hash {
+            eprintln!("{}", self.load(self.program_path().as_str())?);
+            panic!("Program hash did not change after {act}");
+        }
         self.hash = hash;
         Ok(())
     }
@@ -87,7 +86,7 @@ impl Tester {
     }
 
     fn program_write(&self, value: impl Writable) -> RaptorResult<()> {
-        Ok(fs::write(self.program_path(), value.as_string())?)
+        Ok(fs::write(self.program_path(), value.into_string())?)
     }
 
     fn program_hash(&mut self) -> RaptorResult<u64> {
@@ -95,20 +94,20 @@ impl Tester {
     }
 
     fn write(&self, name: &str, value: impl Writable) -> RaptorResult<()> {
-        Ok(fs::write(self.path(name), value.as_string())?)
+        Ok(fs::write(self.path(name), value.into_string())?)
     }
 
     fn touch(&self, name: &str) -> RaptorResult<()> {
         // the hash depends on the mtime, so let enough time pass for the next
         // ctime to be different
         thread::sleep(Duration::from_millis(10));
-        File::open(&self.path(name))?.set_modified(SystemTime::now())?;
+        File::open(self.path(name))?.set_modified(SystemTime::now())?;
 
         Ok(())
     }
 
     fn load(&mut self, name: &str) -> RaptorResult<Arc<Program>> {
-        self.builder.load(&self.path(name))
+        self.builder.load(self.path(name))
     }
 
     fn hash(&mut self, name: &str) -> RaptorResult<u64> {
@@ -121,7 +120,7 @@ impl Tester {
 fn dep_copy() -> RaptorResult<()> {
     let mut test = Tester::setup(["COPY a a"], |test| test.write("a", "1234"))?;
 
-    test.step("change COPY src", |test| test.touch("a"))?;
+    test.step("changing COPY src", |test| test.touch("a"))?;
 
     Ok(())
 }
@@ -130,7 +129,7 @@ fn dep_copy() -> RaptorResult<()> {
 fn dep_render() -> RaptorResult<()> {
     let mut test = Tester::setup(["RENDER a a"], |test| test.write("a", "1234"))?;
 
-    test.step("change RENDER src", |test| test.touch("a"))?;
+    test.step("changing RENDER src", |test| test.touch("a"))?;
 
     Ok(())
 }
@@ -139,7 +138,7 @@ fn dep_render() -> RaptorResult<()> {
 fn dep_self() -> RaptorResult<()> {
     let mut test = Tester::setup([""], |test| test.write("a.rapt", ""))?;
 
-    test.step("change program file", |test| {
+    test.step("changing program file", |test| {
         test.touch(Tester::PROGRAM_NAME)
     })?;
 
