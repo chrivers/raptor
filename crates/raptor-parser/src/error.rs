0dc410c3b602f61277c9ef7f312cc14d275bb413
Christian Iversen <ci@iversenit.dk>
2025-09-20 18:33:21 +0200

    + Added error mapping for lalrpop parser

--- crates/raptor-parser/src/error.rs
+++ crates/raptor-parser/src/error.rs
@@ -1,7 +1,19 @@
 use camino::Utf8PathBuf;
 
+use lalrpop_util::ParseError as LalrParseError;
+use lalrpop_util::lexer::Token;
+
 #[derive(thiserror::Error, Debug)]
 pub enum ParseError {
     #[error("Cannot get parent path from {0:?}")]
     BadPathNoParent(Utf8PathBuf),
+
+    #[error("parse error")]
+    ParseError(String),
+}
+
+impl<'a> From<LalrParseError<usize, Token<'a>, &'a str>> for ParseError {
+    fn from(value: LalrParseError<usize, Token<'a>, &'a str>) -> Self {
+        Self::ParseError(value.to_string())
+    }
 }
