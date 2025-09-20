03cdb3bff649ac0cacc7bd48dbbc29d7c1dfddcf
Christian Iversen <ci@iversenit.dk>
2025-09-20 20:19:41 +0200

    * Adapt lalrpop parser into raptor-parser

--- crates/raptor-parser/src/lib.rs
+++ crates/raptor-parser/src/lib.rs
@@ -1,4 +1,42 @@
 pub mod ast;
 pub mod error;
 pub mod print;
+pub mod unescape;
 pub mod util;
+
+use std::sync::Arc;
+
+use camino::Utf8Path;
+
+use crate::ast::Statement;
+
+pub use error::ParseError;
+pub type ParseResult<T> = Result<T, ParseError>;
+
+pub fn parse(filename: &str, data: &str) -> ParseResult<Vec<Statement>> {
+    let path = Utf8Path::new(filename).to_path_buf();
+    Ok(parser::FileParser::new().parse(&Arc::new(path), data)?)
+}
+
+lalrpop_util::lalrpop_mod!(
+    #[allow(clippy::ptr_arg)]
+    #[allow(clippy::unnested_or_patterns)]
+    #[allow(clippy::redundant_pub_crate)]
+    #[allow(clippy::elidable_lifetime_names)]
+    #[allow(clippy::missing_const_for_fn)]
+    #[allow(clippy::cast_sign_loss)]
+    #[allow(clippy::too_many_lines)]
+    #[allow(clippy::match_same_arms)]
+    #[allow(clippy::must_use_candidate)]
+    #[allow(clippy::option_if_let_else)]
+    #[allow(clippy::no_effect_underscore_binding)]
+    #[allow(clippy::cloned_instead_of_copied)]
+    #[allow(clippy::trivially_copy_pass_by_ref)]
+    #[allow(clippy::needless_raw_string_hashes)]
+    #[allow(clippy::unnecessary_wraps)]
+    #[allow(clippy::use_self)]
+    #[allow(unused_lifetimes)]
+    #[allow(unused_qualifications)]
+    #[rustfmt::skip]
+    pub parser
+);
