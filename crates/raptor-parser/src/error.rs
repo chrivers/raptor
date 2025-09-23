965dfe7bede6199c50842cba3b4cb2f5ae6f1410
Christian Iversen <ci@iversenit.dk>
2025-09-23 20:40:31 +0200

    + Add error variants to ParseError

--- crates/raptor-parser/src/error.rs
+++ crates/raptor-parser/src/error.rs
@@ -1,11 +1,24 @@
 use camino::Utf8PathBuf;
 
 use crate::Rule;
+use crate::lexer::LexerError;
 
 #[derive(thiserror::Error, Debug)]
 pub enum ParseError {
     #[error(transparent)]
     PestError(Box<pest::error::Error<Rule>>),
+
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+
+    #[error(transparent)]
+    LexerError(#[from] LexerError),
+
+    #[error("Unexpected eof")]
+    UnexpectedEof,
+
+    #[error("Expected word")]
+    ExpectedWord,
 
     #[error("Cannot get parent path from {0:?}")]
     BadPathNoParent(Utf8PathBuf),
