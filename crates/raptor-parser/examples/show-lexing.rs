37854c0b97a3c42738db832bd662e6b812ae8c47
Christian Iversen <ci@iversenit.dk>
2025-09-23 07:43:29 +0200

    + Add example showing new lexer in action

---
+++ crates/raptor-parser/examples/show-lexing.rs
@@ -0,0 +1,38 @@
+use std::io::Read;
+use std::io::Write;
+
+use colored::Colorize;
+use log::error;
+use logos::Logos;
+
+use raptor_parser::lexer::WordToken;
+
+fn main() -> Result<(), std::io::Error> {
+    colog::init();
+
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf)?;
+
+    let lexer = WordToken::lexer(&buf);
+
+    let mut stdout = std::io::stdout().lock();
+
+    for token in lexer {
+        match token {
+            Ok(WordToken::Text(txt)) => write!(stdout, "{}", txt.bright_white())?,
+            Ok(WordToken::Newline(txt)) => {
+                write!(stdout, "{txt}")?;
+            }
+            Ok(WordToken::String(txt)) => write!(stdout, "{}", format!("{txt:?}").yellow())?,
+            Ok(WordToken::Comment(txt)) => {
+                writeln!(stdout, "{}", &txt[..txt.len() - 1].dimmed())?;
+            }
+            Ok(WordToken::Whitespace(txt)) => write!(stdout, "{txt}")?,
+            Err(err) => {
+                error!("Lexer error: {err:?}");
+            }
+        }
+    }
+
+    Ok(())
+}
