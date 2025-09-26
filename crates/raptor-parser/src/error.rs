9a3ea96eb914f86f8f9d6d10c9da8faf5c6c51fd
Christian Iversen <ci@iversenit.dk>
2025-09-26 11:17:57 +0200

    * Formatting

--- crates/raptor-parser/src/error.rs
+++ crates/raptor-parser/src/error.rs
@@ -20,7 +20,9 @@ pub enum ParseError {
     #[error(transparent)]
     ParseIntError(#[from] std::num::ParseIntError),
 
-    #[error("Invalid permission mask\n\nValue must specified as 3 or 4 octal digits (0755, 1777, 644, 640, etc)")]
+    #[error(
+        "Invalid permission mask\n\nValue must specified as 3 or 4 octal digits (0755, 1777, 644, 640, etc)"
+    )]
     InvalidPermissionMask,
 
     #[error("Unexpected eof")]
