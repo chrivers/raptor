b3f32ec602bbf2a711f5222b6fbcc5c6226948b5
Christian Iversen <ci@iversenit.dk>
2025-09-28 17:57:44 +0200

    + Generate "raptor.json" for --layers type mounts

--- src/runner/runner.rs
+++ src/runner/runner.rs
@@ -3,12 +3,28 @@ use std::fs;
 use std::hash::BuildHasher;
 
 use camino::{Utf8Path, Utf8PathBuf};
+use serde::{Deserialize, Serialize};
 
 use crate::build::RaptorBuilder;
 use crate::dsl::Program;
 use crate::sandbox::{BindMount, SpawnBuilder};
 use crate::{RaptorError, RaptorResult};
 use raptor_parser::ast::MountType;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct MountsInfo {
+    targets: Vec<String>,
+    layers: HashMap<String, Vec<String>>,
+}
+
+impl MountsInfo {
+    pub fn new() -> Self {
+        Self {
+            targets: Vec::new(),
+            layers: HashMap::new(),
+        }
+    }
+}
 
 pub trait AddMounts: Sized {
     fn add_mounts<S: BuildHasher>(
@@ -40,22 +56,27 @@ impl AddMounts for SpawnBuilder {
                 }
 
                 MountType::Layers => {
-                    let program = builder.load(src)?;
+                    let program = builder.load(&src)?;
+                    let name = program.path.with_extension("").as_str().replace('/', ".");
+
                     let layers = builder.build(program)?;
 
-                    let mut names = vec![];
+                    let mut info = MountsInfo::new();
+
+                    info.targets.push(name.clone());
+
+                    let layer_info = info.layers.entry(name).or_default();
 
                     for layer in &layers {
                         let filename = layer.file_name().unwrap();
-                        names.push(filename);
+                        layer_info.push(filename.to_string());
                         self = self.bind_ro(BindMount::new(layer, mount.dest.join(filename)));
                     }
-                    names.push("");
 
                     let listfile = tempdir.join(format!("mounts-{}", mount.name));
-                    fs::write(&listfile, names.join("\n"))?;
+                    fs::write(&listfile, serde_json::to_string_pretty(&info)? + "\n")?;
 
-                    self = self.bind_ro(BindMount::new(&listfile, mount.dest.join("ORDER")));
+                    self = self.bind_ro(BindMount::new(&listfile, mount.dest.join("raptor.json")));
                 }
 
                 MountType::Overlay => {
