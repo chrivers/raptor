311a7a27de41b1c34a20d42cf8e44fb785281fe1
Christian Iversen <ci@iversenit.dk>
2025-10-24 21:06:21 +0200

    + Add new docker api data models that are more flexible and precise. Supports for both v1 and v2 api

--- crates/dregistry/src/api.rs
+++ crates/dregistry/src/api.rs
@@ -1,6 +1,7 @@
 use std::collections::{BTreeMap, BTreeSet};
 
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 use crate::digest::Digest;
 use crate::error::{DResult, DockerError};
@@ -9,6 +10,27 @@ use crate::error::{DResult, DockerError};
 pub struct DockerTagsList {
     pub name: String,
     pub tags: BTreeSet<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum Manifest {
+    V1(V1Manifest),
+    V2(V2Manifest),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "mediaType")]
+pub enum V2Manifest {
+    #[serde(rename = "application/vnd.oci.image.index.v1+json")]
+    #[serde(alias = "application/vnd.docker.distribution.manifest.list.v2+json")]
+    #[serde(rename_all = "camelCase")]
+    Index { manifests: Vec<DockerManifest> },
+
+    #[serde(rename = "application/vnd.oci.image.manifest.v1+json")]
+    #[serde(alias = "application/vnd.docker.distribution.manifest.v2+json")]
+    #[serde(rename_all = "camelCase")]
+    Manifest(DockerLayers),
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -26,6 +48,48 @@ pub struct DockerLayers {
     pub layers: Vec<DockerLayer>,
     pub media_type: String,
     pub schema_version: u8,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct V1Manifest {
+    pub name: String,
+    pub tag: String,
+    pub architecture: String,
+    pub fs_layers: Vec<V1ManifestLayer>,
+
+    pub history: Vec<V1ManifestHistory>,
+
+    #[serde(default)]
+    pub signatures: Vec<Value>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct V1ManifestHistory {
+    #[serde(with = "serde_nested_json")]
+    pub v1_compatibility: Value,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct V1ManifestLayer {
+    pub blob_sum: Digest,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum MediaType {
+    #[serde(rename = "application/vnd.oci.image.layer.v1.tar+gzip")]
+    #[serde(alias = "application/vnd.docker.image.rootfs.diff.tar.gzip")]
+    ImageLayer,
+
+    #[serde(rename = "application/vnd.oci.image.config.v1+json")]
+    #[serde(alias = "application/vnd.docker.container.image.v1+json")]
+    ImageConfig,
+
+    #[serde(rename = "application/vnd.oci.image.manifest.v1+json")]
+    #[serde(alias = "application/vnd.docker.distribution.manifest.v2+json")]
+    ImageManifest,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
