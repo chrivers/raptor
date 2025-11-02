97b1bc9ad95a3e19779ac7ff455e00a77b520297
chrivers <chrivers@users.noreply.github.com>
2025-11-02 21:41:02 +0000

    deploy: ad87fb69d6b0f46ce29789c99b6ac868d47341e9

--- searcher-9aeb6ddf.js
+++ searcher-9aeb6ddf.js
@@ -436,7 +436,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7babd544.js',
+                path_to_root + 'searchindex-d5d35205.js',
                 'search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
