e037fc774d04d23e897a344fc1b42ec35d137785
Christian Iversen <ci@iversenit.dk>
2025-11-09 22:12:42 +0100

    + Implement Resolver struct, for mapping relative paths and ModuleNames to paths

--- src/program/mod.rs
+++ src/program/mod.rs
@@ -3,9 +3,11 @@ mod executor;
 mod loader;
 mod printer;
 mod resolve;
+mod resolver;
 
 pub use error::*;
 pub use executor::*;
 pub use loader::*;
 pub use printer::*;
 pub use resolve::*;
+pub use resolver::*;
---
+++ src/program/resolver.rs
@@ -0,0 +1,79 @@
+use camino::{Utf8Path, Utf8PathBuf};
+use dashmap::DashMap;
+use dashmap::mapref::one::Ref;
+use raptor_parser::ast::Origin;
+use raptor_parser::util::module_name::{ModuleName, ModuleRoot};
+
+use crate::{RaptorError, RaptorResult};
+
+pub struct Resolver {
+    base: Utf8PathBuf,
+    packages: DashMap<String, Utf8PathBuf>,
+}
+
+impl Resolver {
+    #[must_use]
+    pub fn new(base: Utf8PathBuf) -> Self {
+        Self {
+            base,
+            packages: DashMap::new(),
+        }
+    }
+
+    #[must_use]
+    pub fn base(&self) -> &Utf8Path {
+        &self.base
+    }
+
+    pub fn set_base(&mut self, base: impl AsRef<Utf8Path>) {
+        self.base = base.as_ref().to_path_buf();
+    }
+
+    pub fn add_package(&self, name: String, path: Utf8PathBuf) {
+        self.packages.insert(name, path);
+    }
+
+    #[must_use]
+    pub fn get_package(&self, name: &str) -> Option<Ref<'_, String, Utf8PathBuf>> {
+        self.packages.get(name)
+    }
+
+    #[must_use]
+    pub fn path(&self, path: impl AsRef<Utf8Path>) -> Utf8PathBuf {
+        self.base.join(path)
+    }
+
+    pub fn to_path(
+        &self,
+        root: &ModuleRoot,
+        origin: &Origin,
+        end: &Utf8Path,
+    ) -> RaptorResult<Utf8PathBuf> {
+        let res = match root {
+            ModuleRoot::Relative => origin.path_for(end)?,
+            ModuleRoot::Absolute => end.into(),
+            ModuleRoot::Package(pkg) => {
+                let package = self
+                    .get_package(pkg)
+                    .ok_or_else(|| RaptorError::PackageNotFound(pkg.clone(), origin.clone()))?;
+                package.join(end)
+            }
+        };
+
+        Ok(res)
+    }
+
+    pub fn to_program_path(&self, name: &ModuleName, origin: &Origin) -> RaptorResult<Utf8PathBuf> {
+        let mut end = Utf8PathBuf::new();
+        end.extend(name.parts());
+        end.set_extension("rapt");
+        self.to_path(name.root(), origin, &end)
+    }
+
+    pub fn to_include_path(&self, name: &ModuleName, origin: &Origin) -> RaptorResult<Utf8PathBuf> {
+        let mut end = Utf8PathBuf::new();
+        end.extend(name.parts());
+        end.set_extension("rinc");
+        self.to_path(name.root(), origin, &end)
+    }
+}
